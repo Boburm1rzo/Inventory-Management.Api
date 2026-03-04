e7ec5eb1a9833e2705f31c1c7dda2392fd22739c
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2026-03-04 20:57:50 +0500

    added global exception handler

--- InventoryApp/InventoryApp.Api/Middleware/ExceptionHandlingMiddleware.cs
+++ InventoryApp/InventoryApp.Api/Middleware/ExceptionHandlingMiddleware.cs
@@ -1,5 +1,48 @@
-﻿namespace InventoryApp.Api.Middleware;
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace InventoryApp.Api.Middleware;
 
 public class ExceptionHandlingMiddleware
 {
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Something went wrong: {ex}");
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private Task HandleExceptionAsync(HttpContext context, Exception ex)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = ex switch
+        {
+            ArgumentNullException => (int)HttpStatusCode.BadRequest,
+            UnauthorizedAccessException => (int)HttpStatusCode.Unauthorized,
+            _ => (int)HttpStatusCode.InternalServerError
+        };
+
+        var response = new
+        {
+            StatusCode = context.Response.StatusCode,
+            Message = ex.Message,
+        };
+
+        return context.Response.WriteAsync(JsonConvert.SerializeObject(response));
+    }
 }
--- InventoryApp/InventoryApp.Api/Program.cs
+++ InventoryApp/InventoryApp.Api/Program.cs
@@ -1,5 +1,6 @@
 using InventoryApp.Api.Extention;
 using InventoryApp.Infrastructure.Extentions;
+using Microsoft.AspNetCore.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +17,8 @@ if (app.Environment.IsDevelopment())
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "InventoryApp API V1");
     });
 }
+
+app.UseMiddleware<ExceptionHandlerMiddleware>();
 
 app.UseHttpsRedirection();
 
