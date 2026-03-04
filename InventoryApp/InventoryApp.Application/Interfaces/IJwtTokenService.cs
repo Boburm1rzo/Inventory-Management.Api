18163688764f49278738c8b2bcb7b143ac80d73c
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2026-03-05 00:24:18 +0500

    added jwt service

---
+++ InventoryApp/InventoryApp.Application/Interfaces/IJwtTokenService.cs
@@ -0,0 +1,8 @@
+﻿using InventoryApp.Domain.Entities;
+
+namespace InventoryApp.Application.Interfaces;
+
+public interface IJwtTokenService
+{
+    string GenerateToken(User user, IList<string> roles);
+}
---
+++ InventoryApp/InventoryApp.Infrastructure/Services/JwtTokenService.cs
@@ -0,0 +1,49 @@
+﻿using InventoryApp.Application.Configurations;
+using InventoryApp.Application.Interfaces;
+using InventoryApp.Domain.Entities;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace InventoryApp.Infrastructure.Services;
+
+internal class JwtTokenService(
+    IOptions<JwtSettings> options,
+    ILogger<JwtTokenService> logger) : IJwtTokenService
+{
+    private readonly JwtSettings jwtSettings = options.Value;
+    public string GenerateToken(User user, IList<string> roles)
+    {
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.NameIdentifier,user.Id),
+            new(ClaimTypes.Email,user.Email!),
+            new(ClaimTypes.Name,user.DisplayName)
+        };
+
+        foreach (var role in roles)
+            claims.Add(new Claim(ClaimTypes.Role, role));
+
+        var key = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(jwtSettings.Key));
+
+        var expires = DateTime.UtcNow.AddDays(7);
+
+        var token = new JwtSecurityToken(
+            issuer: jwtSettings.Issuer,
+            audience: jwtSettings.Audience,
+            claims: claims,
+            expires: expires,
+            signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
+        );
+
+        logger.LogInformation(
+            "JWT generated. UserId={UserId} Email={Email} Roles={Roles} ExpiresAtUtc={Expires}",
+            user.Id, user.Email, roles, expires);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
