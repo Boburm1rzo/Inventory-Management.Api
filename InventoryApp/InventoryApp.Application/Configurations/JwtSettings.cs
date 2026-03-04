8aeef9e6bdf716cd337e98f46eeca6590f4ff119
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2026-03-05 00:25:15 +0500

    Create JwtSettings.cs

---
+++ InventoryApp/InventoryApp.Application/Configurations/JwtSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace InventoryApp.Application.Configurations;
+
+public sealed class JwtSettings
+{
+    public const string SectionName = nameof(JwtSettings);
+
+    public string Key { get; set; }
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+}
