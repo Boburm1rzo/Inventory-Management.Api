6135f72035a2cefad6cc7664733d68bac48e0b19
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2026-03-08 01:39:17 +0500

    item service

---
+++ InventoryApp/InventoryApp.Application/Interfaces/IItemService.cs
@@ -0,0 +1,13 @@
+﻿using InventoryApp.Application.DTOs.Item;
+using InventoryApp.Domain.Common;
+
+namespace InventoryApp.Application.Interfaces;
+
+public interface IItemService
+{
+    Task<PagedResult<ItemListItemDto>> GetPagedAsync(int inventoryId, int page, int size);
+    Task<ItemDto> GetByIdAsync(int inventoryId, int itemId);
+    Task<ItemDto> CreateAsync(int inventoryId, CreateItemDto dto);
+    Task<ItemDto> UpdateAsync(int inventoryId, int itemId, UpdateItemDto dto);
+    Task DeleteAsync(int inventoryId, int itemId);
+}
---
+++ InventoryApp/InventoryApp.Infrastructure/Services/ItemService.cs
@@ -0,0 +1,33 @@
+﻿using InventoryApp.Application.DTOs.Item;
+using InventoryApp.Application.Interfaces;
+using InventoryApp.Domain.Common;
+
+namespace InventoryApp.Infrastructure.Services;
+
+internal sealed class ItemService : IItemService
+{
+    public Task<PagedResult<ItemListItemDto>> GetPagedAsync(int inventoryId, int page, int size)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ItemDto> GetByIdAsync(int inventoryId, int itemId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ItemDto> CreateAsync(int inventoryId, CreateItemDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ItemDto> UpdateAsync(int inventoryId, int itemId, UpdateItemDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(int inventoryId, int itemId)
+    {
+        throw new NotImplementedException();
+    }
+}
