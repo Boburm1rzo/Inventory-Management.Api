df1c165d3353b904d2e3a231463bedc6b03185f1
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2026-03-08 00:18:11 +0500

    Update CustomIdGenerator.cs

--- InventoryApp/InventoryApp.Infrastructure/Services/CustomIdGenerator.cs
+++ InventoryApp/InventoryApp.Infrastructure/Services/CustomIdGenerator.cs
@@ -1,9 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using InventoryApp.Application.Interfaces;
+using InventoryApp.Domain.Entities;
+using InventoryApp.Domain.Enums;
 using System.Text;
 
 namespace InventoryApp.Infrastructure.Services;
 
-internal class CustomIdGenerator
+internal sealed class CustomIdGenerator : ICustomIdGenerator
 {
+    public string Generate(List<InventoryIdFormatPart> parts, int nextSequence)
+    {
+        var sb = new StringBuilder();
+
+        foreach (var part in parts.OrderBy(x => x.Order))
+        {
+            sb.Append(part.Type switch
+            {
+                IdFormatPartType.FixedText => part.Config ?? "",
+                IdFormatPartType.Sequence => nextSequence.ToString(part.Config ?? "000"),
+                IdFormatPartType.Random6Digit => Random.Shared.Next(100000, 999999).ToString(),
+                IdFormatPartType.Random9Digit => Random.Shared.Next(100000000, 999999999).ToString(),
+                IdFormatPartType.Guid => Guid.NewGuid().ToString("N")[..8].ToUpper(),
+                IdFormatPartType.DateTime => DateTime.UtcNow.ToString(part.Config ?? "yyyyMMdd"),
+                _ => ""
+            });
+        }
+
+        return sb.ToString();
+    }
 }
