b0b4418f93e1f19b0a7612ef389ba025fc47fa67
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2026-03-10 16:44:33 +0500

    Create AccessController.cs

---
+++ InventoryApp/InventoryApp.Api/Controllers/AccessController.cs
@@ -0,0 +1,37 @@
+﻿using InventoryApp.Application.DTOs.Access;
+using InventoryApp.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryApp.Api.Controllers;
+
+[ApiController]
+[Route("/api/inventories/{inventoryId:int}/access")]
+public class AccessController(IInventoryAccessService service) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAccessList(int inventoryId)
+    {
+        var result = await service.GetAccessListAsync(inventoryId);
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    [Authorize]
+    public async Task<IActionResult> AddAccess(int inventoryId, AddAccessDto dto)
+    {
+        await service.AddAccessAsync(inventoryId, dto);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{userId}")]
+    [Authorize]
+    public async Task<IActionResult> Delete(int inventoryId, string userId)
+    {
+        await service.RemoveAccessAsync(inventoryId, userId);
+
+        return NoContent();
+    }
+}
