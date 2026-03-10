9ebc813c929a36861de8fbd2fb7df5ff1ca7e14f
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2026-03-10 16:44:16 +0500

    Update UsersController.cs

--- InventoryApp/InventoryApp.Api/Controllers/UsersController.cs
+++ InventoryApp/InventoryApp.Api/Controllers/UsersController.cs
@@ -1,5 +1,16 @@
-﻿namespace InventoryApp.Api.Controllers;
+﻿using InventoryApp.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
-public class UsersController
+namespace InventoryApp.Api.Controllers;
+
+[ApiController]
+[Route("/api/users")]
+public class UsersController(IInventoryAccessService inventoryAccessService) : ControllerBase
 {
+    [HttpGet("search")]
+    public async Task<IActionResult> SearchUsers([FromQuery] string query)
+    {
+        var results = await inventoryAccessService.SearchUsersAsync(query);
+        return Ok(results);
+    }
 }
