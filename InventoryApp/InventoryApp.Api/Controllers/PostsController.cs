8aacf3e190fa9c8546268a5f666572b2ca2f137d
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2026-03-10 16:44:27 +0500

    Update PostsController.cs

--- InventoryApp/InventoryApp.Api/Controllers/PostsController.cs
+++ InventoryApp/InventoryApp.Api/Controllers/PostsController.cs
@@ -1,5 +1,37 @@
-﻿namespace InventoryApp.Api.Controllers;
+﻿using InventoryApp.Application.DTOs.Post;
+using InventoryApp.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
-public class PostsController
+namespace InventoryApp.Api.Controllers;
+
+[ApiController]
+[Route("api/inventories/{inventoryId:int}/posts")]
+public class PostsController(IPostService service) : ControllerBase
 {
+    [HttpGet]
+    public async Task<IActionResult> GetAll(int inventoryId)
+    {
+        var result = await service.GetPostsAsync(inventoryId);
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    [Authorize]
+    public async Task<IActionResult> Create(int inventoryId, CreatePostDto dto)
+    {
+        var result = await service.CreatePostAsync(inventoryId, dto);
+
+        return Ok(result);
+    }
+
+    [HttpDelete("{postId:int}")]
+    [Authorize]
+    public async Task<IActionResult> Delete(int inventoryId, int postId)
+    {
+        await service.DeletePostAsync(inventoryId, postId);
+
+        return NoContent();
+    }
 }
