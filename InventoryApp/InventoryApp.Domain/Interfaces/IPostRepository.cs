bc87e6b5e9aed1db7d0e667079cfaadfb7c1d97c
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2026-03-10 16:40:10 +0500

    removed unused files

--- InventoryApp/InventoryApp.Domain/Interfaces/IPostRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using InventoryApp.Domain.Entities;
-
-namespace InventoryApp.Domain.Interfaces;
-
-public interface IPostRepository
-{
-    Task<List<Post>> GetByInventoryAsync(int inventoryId);
-    Task AddAsync(Post post);
-}
