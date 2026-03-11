e1fb6539987de32ecf18d67a8fa8584ad4a6e013
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2026-03-11 23:05:29 +0500

    Update Program.cs

--- InventoryApp/InventoryApp.Api/Program.cs
+++ InventoryApp/InventoryApp.Api/Program.cs
@@ -4,48 +4,65 @@ using InventoryApp.Api.Middleware;
 using InventoryApp.Infrastructure.Extentions;
 using InventoryApp.Infrastructure.Seeders;
 using Serilog;
+using Serilog.Events;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.Host.UseSerilog((ctx, lc) =>
-    lc.ReadFrom.Configuration(ctx.Configuration)
-      .Enrich.FromLogContext());
-
-builder.Services.AddInfrastructure(builder.Configuration);
-builder.Services.AddApi(builder.Configuration);
-builder.Services.AddProblemDetails();
-
-var app = builder.Build();
-
-app.UseSwagger();
-app.UseSwaggerUI(options =>
+try
 {
-    options.SwaggerEndpoint("/swagger/v1/swagger.json", "InventoryApp API V1");
-});
+    Log.Information("Starting web host...");
 
+    var builder = WebApplication.CreateBuilder(args);
 
-app.UseMiddleware<ExceptionHandlingMiddleware>();
+    builder.Host.UseSerilog((ctx, lc) =>
+        lc.ReadFrom.Configuration(ctx.Configuration)
+          .WriteTo.Console()
+          .Enrich.FromLogContext());
 
-app.UseSerilogRequestLogging(options =>
+    builder.Services.AddInfrastructure(builder.Configuration);
+    builder.Services.AddApi(builder.Configuration);
+    builder.Services.AddProblemDetails();
+
+    var app = builder.Build();
+
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "InventoryApp API V1");
+    });
+
+    app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+    app.UseSerilogRequestLogging(options =>
+    {
+        options.MessageTemplate = "HTTP {RequestMethod} {RequestPath} => {StatusCode} in {Elapsed:0.0000} ms";
+    });
+
+    app.UseHttpsRedirection();
+    app.UseRouting();
+    app.UseCors("Frontend");
+    app.UseCookiePolicy();
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapHub<DiscussionHub>("/hubs/discussion");
+    app.MapControllers();
+
+    using (var scope = app.Services.CreateScope())
+        await DatabaseSeeder.SeedAsync(scope.ServiceProvider);
+
+    app.Run();
+}
+catch (Exception ex)
 {
-    options.MessageTemplate = "HTTP {RequestMethod} {RequestPath} => {StatusCode} in {Elapsed:0.0000} ms";
-});
-
-app.UseHttpsRedirection();
-
-app.UseRouting();
-
-app.UseCors("Frontend");
-
-app.UseCookiePolicy();
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapHub<DiscussionHub>("/hubs/discussion");
-app.MapControllers();
-
-using (var scope = app.Services.CreateScope())
-    await DatabaseSeeder.SeedAsync(scope.ServiceProvider);
-
-
-app.Run();
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
