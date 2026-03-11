180217807899d688950ba304d82d1405d2fad463
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2026-03-11 22:50:12 +0500

    added personla service

---
+++ InventoryApp/InventoryApp.Application/Interfaces/IPersonalService.cs
@@ -0,0 +1,8 @@
+﻿using InventoryApp.Application.DTOs.Personal;
+
+namespace InventoryApp.Application.Interfaces;
+
+public interface IPersonalService
+{
+    Task<PersonalStatsDto> GetMyStatsAsync();
+}
---
+++ InventoryApp/InventoryApp.Infrastructure/Services/PersonalService.cs
@@ -0,0 +1,36 @@
+﻿using InventoryApp.Application.DTOs.Personal;
+using InventoryApp.Application.Interfaces;
+using InventoryApp.Application.Mappers;
+using InventoryApp.Infrastructure.Persistance;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryApp.Infrastructure.Services;
+
+internal sealed class PersonalService(
+    ICurrentUserService currentUserService,
+    AppDbContext context) : IPersonalService
+{
+    public async Task<PersonalStatsDto> GetMyStatsAsync()
+    {
+        var userId = currentUserService.UserId;
+
+        var inventories = await context.Inventories
+            .Where(x => x.OwnerId == userId)
+            .Include(x => x.Items)
+            .ToListAsync();
+
+        var totalInventories = inventories.Count;
+        var totalItems = inventories.Sum(x => x.Items.Count);
+
+        var totalLikes = await context.Likes
+            .Where(x => x.Item.InventoryId == context.Inventories
+                .Where(i => i.OwnerId == userId)
+                .Select(i => i.Id)
+                .FirstOrDefault())
+            .CountAsync();
+
+        var inventortDtos = inventories.Select(x => x.MapToListItemDto()).ToList();
+
+        return new(totalInventories, totalItems, totalLikes, inventortDtos);
+    }
+}
