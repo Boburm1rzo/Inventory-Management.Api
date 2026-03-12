d3a5cd1be249f348c8255b0b03e48a18808b3c2b
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2026-03-12 21:31:37 +0500

    Update DependencyInjection.cs

--- InventoryApp/InventoryApp.Api/Extention/DependencyInjection.cs
+++ InventoryApp/InventoryApp.Api/Extention/DependencyInjection.cs
@@ -23,6 +23,13 @@ public static class DependencyInjection
         var authSettings = configuration.GetSection(AuthenticationSettings.SectionName).Get<AuthenticationSettings>();
         var jwtSettings = configuration.GetSection(JwtSettings.SectionName).Get<JwtSettings>();
 
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($" ################### google clientId: {authSettings.GoogleClientId} ###################");
+        Console.WriteLine($" ################### google Secret : {authSettings.GoogleClientSecret} ###################");
+        Console.WriteLine($" ################### facebook ApiId: {authSettings.FacebookAppId} ###################");
+        Console.WriteLine($" ################### facebook Secret: {authSettings.FacebookAppSecret} ###################");
+        Console.ResetColor();
+
         services.AddAuthentication(options =>
         {
             options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
