6d0ac10857ac4407e880535a9ad7bf5fdbf13729
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2026-03-15 01:27:54 +0500

    Create TagsController.cs

---
+++ InventoryApp/InventoryApp.Api/Controllers/TagsController.cs
@@ -0,0 +1,13 @@
+﻿using InventoryApp.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryApp.Api.Controllers;
+
+[ApiController]
+[Route("api/tags")]
+public class TagsController(ITagService service) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAll([FromQuery] string? prefix = null)
+        => Ok(await service.GetAsync(prefix));
+}
