165481f1fe781884eaf887c8653579216a807887
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2026-03-15 01:29:19 +0500

    Create CategoriesController.cs

---
+++ InventoryApp/InventoryApp.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,46 @@
+﻿using InventoryApp.Application.DTOs.Category;
+using InventoryApp.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryApp.Api.Controllers;
+
+[ApiController]
+[Route("api/categories")]
+public class CategoriesController(ICategoryService service) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var results = await service.GetAllAsync();
+
+        return Ok(results);
+    }
+
+    [HttpPost()]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> Create(CreateCategoryDto dto)
+    {
+        var result = await service.CreateAsync(dto);
+
+        return CreatedAtAction(nameof(GetAll), new { result.Id }, result);
+    }
+
+    [HttpPut("{id:int}")]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> Update(int id, UpdateCategoryDto dto)
+    {
+        var result = await service.UpdateAsync(id, dto);
+
+        return Ok(result);
+    }
+
+    [HttpDelete("{id:int}")]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        await service.DeleteAsync(id);
+
+        return NoContent();
+    }
+}
