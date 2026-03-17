0c0a665533da7a75dfd5faa0148c77a708a68ffd
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2026-03-17 22:45:42 +0500

    Update BlockedUserMiddleware.cs

--- InventoryApp/InventoryApp.Api/Middleware/BlockedUserMiddleware.cs
+++ InventoryApp/InventoryApp.Api/Middleware/BlockedUserMiddleware.cs
@@ -1,5 +1,38 @@
-﻿namespace InventoryApp.Api.Middleware;
+﻿using InventoryApp.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 
-public class BlockedUserMiddleware
+namespace InventoryApp.Api.Middleware;
+
+public class BlockedUserMiddleware(RequestDelegate next)
 {
+    public async Task InvokeAsync(HttpContext context, UserManager<User> userManager)
+    {
+        if (context.User.Identity?.IsAuthenticated == true)
+        {
+
+            var userId = context.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (userId is not null)
+            {
+                var user = await userManager.FindByIdAsync(userId);
+
+                if (user?.IsBlocked == true)
+                {
+                    context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                    context.Response.ContentType = "application/json";
+
+                    await context.Response.WriteAsJsonAsync(new
+                    {
+                        StatusCode = 403,
+                        Message = "Your account has been blocked."
+                    });
+
+                    return;
+                }
+            }
+        }
+
+        await next(context);
+    }
 }
